a8f597a9f8fe64d6661d31b28b8af840e0ed5fa5
Matteo Chini <matteo.chini@gmail.com>
2025-05-23 16:42:32 +0200

    confronto start

--- src/app/pages/simulazioni/scenari/scenari.component.ts
+++ src/app/pages/simulazioni/scenari/scenari.component.ts
@@ -13,9 +13,41 @@ export class ScenariComponent {
   scenari: Scenario[] = [];
   loading = true;
   simulationId: any;
-
+  comparazioneAttiva = false;
+  selectedScenari: any[] = [];
   constructor(private scenarioService: ScenarioService,private route: ActivatedRoute) {}
+  toggleComparazione() {
+    this.comparazioneAttiva = !this.comparazioneAttiva;
+    if (!this.comparazioneAttiva) {
+      this.selectedScenari = [];
+    }
+  }
+  
+  isScenarioSelected(scenario: any): boolean {
+    return this.selectedScenari.some(s => s.id === scenario.id);
+  }
+  
+  checkboxStates: { [id: string]: boolean } = {};
 
+  onScenarioCheck(scenario: any, checked: boolean) {
+    if (checked) {
+      if (this.selectedScenari.length < 2 && !this.isScenarioSelected(scenario)) {
+        this.selectedScenari.push(scenario);
+      } else {
+        // Se sono già 2, non accettare altri
+        this.checkboxStates[scenario.id] = false;
+      }
+    } else {
+      this.selectedScenari = this.selectedScenari.filter(s => s.id !== scenario.id);
+    }
+  }
+  
+  confrontaScenari() {
+    // Implementa la logica di confronto (es: naviga a una pagina di confronto)
+    // Esempio:
+    // this.router.navigate(['/simulazioni', simulationId, 'confronta', selectedScenari[0].id, selectedScenari[1].id]);
+    alert('Confronto tra: ' + this.selectedScenari.map(s => s.name).join(' e '));
+  }
   ngOnInit(): void {
     this.simulationId = this.route.snapshot.paramMap.get('simulationId')!;
 
