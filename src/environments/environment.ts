61535a6e859a634f0aa1ad4de41d5812a8a9c290
Matteo Chini <matteo.chini@gmail.com>
2025-09-11 15:13:08 +0200

    new error chart

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'https://overtourism.digitalhub-test.smartcommunitylab.it/api/v1'
+    apiBaseUrl: 'http://127.0.0.1:8000/api/v1'
   };
