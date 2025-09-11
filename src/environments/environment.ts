65cb911d0afe84f17e299a745ac7ad25a7645577
Matteo Chini <matteo.chini@gmail.com>
2025-09-11 15:27:10 +0200

    refixed env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiBaseUrl: 'http://127.0.0.1:8000/api/v1'
-  };
+  production: false,
+  apiBaseUrl: 'https://overtourism.digitalhub-test.smartcommunitylab.it/api/v1'
+};
