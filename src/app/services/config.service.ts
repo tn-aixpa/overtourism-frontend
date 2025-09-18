6fc2ff5cf2ae04b18488c58370f7b4ec13a4df35
Matteo Chini <matteo.chini@gmail.com>
2025-09-18 11:09:08 +0200

    fixed config env var

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,6 @@ export class ConfigService {
   }
 
   get apiBaseUrl(): string {
-    return this.config?.apiBaseUrl || '';
+    return environment.apiBaseUrl || '';
   }
 }
