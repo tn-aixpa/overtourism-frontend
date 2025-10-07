7516e6eeb5cf429183ac7554e899b54d3522bb8f
Matteo Chini <matteo.chini@gmail.com>
2025-10-07 11:29:04 +0200

    fixed reload app with nav item and style

--- src/app/components/app-header/app-header.module.ts
+++ src/app/components/app-header/app-header.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AppHeaderComponent } from './app-header.component';
 import { DesignAngularKitModule } from 'design-angular-kit';
 import { TranslateModule } from '@ngx-translate/core';
+import {  RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppHeaderComponent],
   imports: [CommonModule, DesignAngularKitModule,TranslateModule.forRoot(),
+    RouterModule
   ],
   exports: [AppHeaderComponent]
 })
